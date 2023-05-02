b835f6ae9dad63ded9d6bdefdfde7190950bcb03
Daevah Taylaran <daevtayl@gmail.com>
2023-05-02 10:23:56 -0700

    bubble sort

--- bubble-sort.py
+++ bubble-sort.py
@@ -10,11 +10,17 @@ def bubble_sort(list_a):
     # this will be used inside the control flow to break out from loop once the list has been sorted
 
     while not sorted:
+    # as long as the sorted variable is False
         sorted = True
+
         for i in range(0, indexing_length):
+            # we are targeting the indexing_length variable 
             if list_a[i] > list_a[i+1]:
+                #we are comparing the value from the left to the value to the right at each position iterated
                 sorted = False
                 list_a[i], list_a[i+1] = list_a[i+1], list_a[i]
+                #what we are saying here is if the value to the left is greater than the value to the right, then the values switch
+            #once all items have been sorted, the if statement won't activate and the sorted variable will remain True, which will break the while loop
     return list_a
 
 
