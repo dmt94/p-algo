630c4d0d428678c25b3a225b3d59d0fb340adcae
Daevah Taylaran <daevtayl@gmail.com>
2023-05-03 08:59:16 -0700

    more bubble sort

--- bubble-sort.py
+++ bubble-sort.py
@@ -17,10 +17,11 @@ def bubble_sort(list_a):
             # we are targeting the indexing_length variable 
             if list_a[i] > list_a[i+1]:
                 #we are comparing the value from the left to the value to the right at each position iterated
+                # within this loop, we compare each pair adjaent values and swap those values if they're out of order and switch sorted to Talse
                 sorted = False
                 list_a[i], list_a[i+1] = list_a[i+1], list_a[i]
                 #what we are saying here is if the value to the left is greater than the value to the right, then the values switch
-            #once all items have been sorted, the if statement won't activate and the sorted variable will remain True, which will break the while loop
+    #once all items have been sorted, the if statement won't activate and the sorted variable will remain True, which will break the while loop
     return list_a
 
 
